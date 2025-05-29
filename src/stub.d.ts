5be030d89029d34b1f031db6dfae809e3c46ba23
Nir Tamir <nirtamir2@gmail.com>
2025-05-29 19:43:22 +0300

    WIP - feat: Add eslint-plugin-react-you-might-not-need-an-effect

--- src/stub.d.ts
+++ src/stub.d.ts
@@ -2,3 +2,4 @@ declare module "eslint-plugin-react-hooks";
 declare module "eslint-plugin-github";
 declare module "eslint-plugin-react-refresh";
 declare module "eslint-plugin-tailwindcss";
+declare module 'eslint-plugin-react-you-might-not-need-an-effect';
